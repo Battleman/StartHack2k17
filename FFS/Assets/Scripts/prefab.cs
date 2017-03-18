217fe340d117ca5e9c0d83d7efa216e045b2f04c
flyingdutchman <mika.pagani@heig-vd.ch>
2017-03-18 15:37:12 +0100

    Implemented Magnet and Spawn in FFS

---
+++ FFS/Assets/Scripts/prefab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefab : MonoBehaviour {
+
+    public GameObject cubePrefab;
+    GameObject cubePrefabClone;
+    
+
+	void Update () {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                cubePrefabClone = Instantiate(cubePrefab, hit.point, Quaternion.identity) as GameObject;
+            }
+
+        }
+    }
+}
